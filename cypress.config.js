09917ffa8bac69841c8132ab6269c5444f65321e
Artur <atrur@Atrurs-MacBook-Air.local>
2026-04-06 13:24:13 +0200

    init project

---
+++ cypress.config.js
@@ -0,0 +1,11 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  allowCypressEnv: false,
+
+  e2e: {
+    baseUrl: 'https://www.saucedemo.com/',
+    setupNodeEvents(on, config) {
+    },
+  },
+});
