cc2d5873d382b1f41fd19900671c218fa7da5684
Artur <atrur@Atrurs-MacBook-Air.local>
2026-04-13 17:50:39 +0200

    pom

--- cypress/integration/login_pom.cy.js
+++ cypress/integration/login_pom.cy.js
@@ -1,9 +1,29 @@
-describe('Login', ()=> {
-    it('Login HP', ()=> {
-        cy.visit('')
-        cy.get('#user-name').clear().type('standard_user')
-        cy.get('#password').clear().type('secret_sauce')
-        cy.get('#login-button').click()
-        cy.url().should('include', 'inventory')
+import User from '../fixtures/users.json'
+import LoginPage from '../pom/login_page'
+import MainPage from '../pom/main_page'
+
+const USER = User.standard_user,
+    USER_SD = User.locked_out_user,
+    ERR_MSG = 'Epic sadface: Sorry, this user has been locked out.'
+
+describe('Login', () => {
+    it('Login HP', () => {
+        cy.log('WHEN User goes to the Login Page')
+        LoginPage.open()
+        cy.log('And input username')
+        LoginPage.usernameInputField.clear().type(USER.username)
+        cy.log('And input password')
+        LoginPage.passwordInputField.clear().type(USER.password)
+        cy.log('And hit [Login]')
+        LoginPage.loginButton.click()
+        cy.log('Then the Main Page is opened')
+        MainPage.isMainPage
+    })
+
+    it('Login SD', () => {
+        cy.log('WHEN Locked User tries to login')
+        LoginPage.login(USER_SD)
+        cy.log('Then the error msg is shown')
+        MainPage.errorMsg.should('contain.text', ERR_MSG)
     })
 })
--- cypress/pom/login_page.js
+++ cypress/pom/login_page.js
@@ -1,5 +1,32 @@
 class LoginPage {
 
+    open() {
+        cy.visit('')
+    }
+
+    get usernameInputField() {
+        return cy.get('#user-name')
+    }
+
+    get passwordInputField() {
+        return cy.get('#password')
+    }
+
+    get loginButton() {
+        return cy.get('#login-button')
+    }
+
+    login(user) {
+        this.open()
+        this.usernameInputField.clear().type(user.username)
+        this.passwordInputField.clear().type(user.password)
+        this.loginButton.click()
+    }
+
+    isLoginPage() {
+        cy.url().should('include', 'inventory')
+    }
+
 }
 
 export default new LoginPage()
