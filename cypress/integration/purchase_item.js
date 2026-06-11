4bedc0746d8e15d74793f166a9cababc8aa44733
Artur <atrur@Atrurs-MacBook-Air.local>
2026-06-11 18:32:44 +0200

    add array users

--- cypress/integration/purchase_item.js
+++ cypress/integration/purchase_item.js
@@ -5,31 +5,37 @@ import CartPage from '../pom/cart_page'
 import { calculateTotalPrice } from '../support/utils'
 
 const USER = User.standard_user
+const USER_2 = User.standard_user_2
+const USERS = [USER, USER_2, USER, USER_2]
 
 describe('Buy Products', () => {
-    beforeEach(() => {
-        cy.log('GIVEN a User is signed in')
-        cy.login(USER)
-    })
+    USERS.forEach(user => {
 
-    it('Buy Products HP', () => {
-        cy.log('WHEN the User adds products in the cart')
-        // cy.addProductsToCartFlaky(Products)
-        // cy.addMultipleProductsToCart(Products.length)
-        cy.addProductsToCartStable(Products)
-        cy.getProductPrices(Products).then((prices) => {
-            const TOTAL_PRICE = calculateTotalPrice(prices, USER.tax)
-
-            cy.log('THEN the products are added')
-            InventoryPage.cartIcon.scrollIntoView().should('have.text', Products.length).click()
-            cy.checkNumberOfProducts(Products.length)
-            cy.log('AND WHEN the User checks out the purchase')
-            CartPage.checkoutButton.click()
-            cy.fillInCheckoutForm(USER)
-            CartPage.continueButton.click()
-            cy.log('THEN the the Total price is correct')
-            cy.log(TOTAL_PRICE)
-            CartPage.totalPrice.scrollIntoView().should('contain.text', TOTAL_PRICE)
+        beforeEach(() => {
+            cy.log('GIVEN a User is signed in')
+            cy.login(user)
+        })
+        
+        it(`Buy Products HP ${user.last_name}`, () => {
+            cy.log(user.last_name)
+            cy.log('WHEN the User adds products in the cart')
+            // cy.addProductsToCartFlaky(Products)
+            // cy.addMultipleProductsToCart(Products.length)
+            cy.addProductsToCartStable(Products)
+            cy.getProductPrices(Products).then((prices) => {
+                const TOTAL_PRICE = calculateTotalPrice(prices, user.tax)
+                
+                cy.log('THEN the products are added')
+                InventoryPage.cartIcon.scrollIntoView().should('have.text', Products.length).click()
+                cy.checkNumberOfProducts(Products.length)
+                cy.log('AND WHEN the User checks out the purchase')
+                CartPage.checkoutButton.click()
+                cy.fillInCheckoutForm(user)
+                CartPage.continueButton.click()
+                cy.log('THEN the the Total price is correct')
+                cy.log(TOTAL_PRICE)
+                CartPage.totalPrice.scrollIntoView().should('contain.text', TOTAL_PRICE)
+            })
         })
     })
 })
